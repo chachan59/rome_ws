d1b51b9c8e4a3002c32042375ca76f39d5398d89
chalearmrat chandee <62545853+chachan59@users.noreply.github.com>
2020-04-19 16:16:52 +0700

    Add files via upload

---
+++ build/rome_gazebo/CMakeFiles/mynode.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mynode.dir/src/mynode.cpp.o"
+  "/home/noah/rome_ws/devel/lib/rome_gazebo/mynode.pdb"
+  "/home/noah/rome_ws/devel/lib/rome_gazebo/mynode"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mynode.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/rome_gazebo/CMakeFiles/mynode1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mynode1.dir/src/mynode1.cpp.o"
+  "/home/noah/rome_ws/devel/lib/rome_gazebo/mynode1.pdb"
+  "/home/noah/rome_ws/devel/lib/rome_gazebo/mynode1"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mynode1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
